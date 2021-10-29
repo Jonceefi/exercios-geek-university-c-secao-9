a2f04d5522f902857b5138300173fb5d65b31021
Jonceefi <78228991+Jonceefi@users.noreply.github.com>
2021-10-29 10:21:59 -0300

    Rename Ex 014 - Dicas de consumo..c to Ex 014 - Dicas de consumo.c

--- Ex 014 - Dicas de consumo.c
+++ Ex 014 - Dicas de consumo.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-void consumo(float,float); //cabeÁalho da funÁ„o
+void consumo(float,float); //cabe√ßalho da fun√ß√£o
 
 int main(int argc, char** argv)
 {
 	float n1,n2,cons;
-	printf("Digite a dist‚ncia:\n");
+	printf("Digite a dist√¢ncia:\n");
 	scanf("%f",&n1);
 	printf("Digite os litros conumidos no percurso:\n");
 	scanf("%f",&n2);
@@ -20,16 +20,16 @@ int main(int argc, char** argv)
 void consumo(float n1, float n2) 
 {
 	float cons;
-	//Fazendo o consumo mÈdio do veÌculo:
+	//Fazendo o consumo m√©dio do ve√≠culo:
 	cons = (n1/n2);
-	//Condicional para ‡s dicas:
+	//Condicional para √†s dicas:
 	if (cons < 8)
 		printf("\nVenda esse carro!\n");		 
 	else if (cons >= 8 && cons <= 14)
 	{
-		printf("\nEconÙmico!\n");
+		printf("\nEcon√¥mico!\n");
 	}
 	else
-		printf("\nSuper EconÙmico!!!\n");
+		printf("\nSuper Econ√¥mico!!!\n");
 }
 
